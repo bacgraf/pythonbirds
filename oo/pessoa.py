ce7cfdfe115f1a1b4f09b9fe71e1a1a4600d35ea
bacgraf <bacgraf@gmail.com>
2021-04-26 17:07:32 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,12 @@
-class Pessoa:
-    def __init__(self, *filhos, nomme = None, idade = 35):
-        self.idade = idade
-        self.nome = nomme
-        self.filhos = list(filhos)
+class Pessoa:                   #classe
+    olhos = 2                   #atributo de classe
 
-    def cumprimentar(self):
+    def __init__(self, *filhos, nomme = None, idade = 35): #Objeto com atributos sendo *filhos um atributo complexo
+        self.idade = idade                #Variáveis do objeto
+        self.nome = nomme                 #Variáveis do objeto
+        self.filhos = list(filhos)        #Variável complexa do objeto
+
+    def cumprimentar(self):    #método
         return f"ola {id(self)}"
 
 if __name__ == '__main__':
@@ -22,5 +24,11 @@ if __name__ == '__main__':
     adobe.sobrenome = 'RML'
     print(adobe.__dict__)
     del adobe.filhos
+    adobe.olhos = 1
+    del adobe.olhos
     print(adobe.__dict__)
     print(borel.__dict__)
+    print(Pessoa.olhos)
+    print(adobe.olhos)
+    print(corel.olhos)
+    print(id(Pessoa.olhos), id(adobe.olhos), id(corel.olhos))
